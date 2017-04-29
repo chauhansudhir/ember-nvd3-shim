8e27eeb46517289ed463b5b4857cb6a770162bce
Sudhir Chauhan <chauhan.sudhir79@yahoo.com>
2017-04-28 17:05:19 -0700

    d3 chart loading issue

--- index.js
+++ index.js
@@ -1,27 +1,25 @@
-/* jshint node: true */
-'use strict';
-
+/*jshint node:true*/
 module.exports = {
-  name: 'ember-nvd3-shim',
-  included: function included(app) {
-    app.import(app.bowerDirectory + '/d3/d3.js');
-    if (process.env.EMBER_CLI_FASTBOOT !== 'true') {
+  description: 'Installation blueprint for ember-nvd3-shim',
+  normalizeEntityName: function() {},
 
-      app.import(app.bowerDirectory + '/nvd3/build/nv.d3.js');
+  afterInstall: function() {
+    var self = this;
 
-      var options1 = app.options['ember-cli-nvd3'];
-      var options2 = app.options['ember-nvd3-shim'];
-
-      if ((options1 === undefined && options2 === undefined) || // if both options are not present
-         (options1 && options1.includeCss !== false) ||
-         (options2 && options2.includeCss !== false)) {
-        app.import(app.bowerDirectory + '/nvd3/build/nv.d3.css');
-      }
-      app.import('vendor/ember-nvd3.js', {
-        exports: {
-          nv: ['default']
-        }
+    return this.addBowerPackageToProject('nvd3','^1.8.0')
+      .then(function () {
+        return self.addAddonToProject('brzpegasus/ember-d3#v3');
       });
-    }
   }
+
+  // locals: function(options) {
+  //   // Return custom template variables here.
+  //   return {
+  //     foo: options.entity.options.foo
+  //   };
+  // }
+
+  // afterInstall: function(options) {
+  //   // Perform extra work here.
+  // }
 };
